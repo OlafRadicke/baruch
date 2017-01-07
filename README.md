8316c8c507452fdf0e4ddfa9439fd9c6bf2418e7
Olaf Radicke <briefkasten@olaf-radicke.de>
2017-01-07 23:31:04 +0100

    Generating first control file and make a first build

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ Starting
 # Requirements #
 
 - Go interpreter
-- ...
+- dpkg-deb
+- fakeroot
 
 # Test #
 
@@ -31,3 +32,7 @@ go run main.go
 # Example #
 
 You find a spec.json file example in the directory "example".
+
+# External documentation #
+
+- [How_to_create_an_RPM_package](https://fedoraproject.org/wiki/How_to_create_an_RPM_package)
--- main.go
+++ main.go
@@ -13,47 +13,88 @@ import (
 
 func main() {
     fmt.Println("Start...")
-    readJson()
-    buildDeb()
+    var spec = readJson()
+    buildDeb(spec)
     fmt.Println("...ready.")
 }
 
+func createDistDir() {
 
-func buildDeb() {
-  // fakeroot dpkg-deb --build debian
-    fmt.Printf("\nBuild deb...")
-    out, err := exec.Command("dpkg-deb", "--build", "./dist/demo_0.1-2_i386/").Output()
-    if err != nil {
-        log.Fatal(err)
-  	    fmt.Printf("\n Error build deb: %s\n", err)
-    }
-    fmt.Printf("The date is %s\n", out)
+
 }
 
+func getBuildRoot(spec SpecStruct)(buildRoot string){
+    buildRoot = spec.BuildRoot + "/"+ spec.Name + "_" + spec.Version + "-" + spec.Release + "_" + spec.BuildArch
+    return
+}
 
-func readJson() {
+func writeControleFile(spec SpecStruct){
+//  d1 := []byte("hello\ngo\n")
+    var content = "Package: " + spec.Name + "\n"
+    content += "Version: " + spec.Version + "-" + spec.Release + "\n"
+    content += "Section: " + spec.Group + "\n"
+    content += "Priority: " + spec.Priority + "\n"
+    content += "Architecture: " + spec.BuildArch + "\n"
+    content += "Maintainer: " + spec.Maintainer + "\n"
+    content += "Description: " + spec.Description + "\n"
+    var buildRoot = getBuildRoot(spec)
+    var controlFileName = buildRoot + "/DEBIAN/control"
 
-  type ChangeLog struct {
+    err := os.MkdirAll(buildRoot + "/DEBIAN", 0777)
+    if err != nil {
+        fmt.Printf("\nError by creating directory: %s\n", err.Error())
+        log.Fatal(err)
+    }
+    err = ioutil.WriteFile(controlFileName, []byte(content), 0644)
+  //  err = ioutil.WriteFile("/tmp/test.muell", []byte(content), 0644)
+    if err != nil {
+        fmt.Printf("\nError by writting file: %s\n", err.Error())
+        log.Fatal(err)
+    }
+    fmt.Printf("\nWritting  control file in: %s\n", controlFileName)
+}
+
+func buildDeb(spec SpecStruct) {
+    fmt.Printf("\nBuild deb [%s]\n", spec.Name)
+    writeControleFile(spec)
+    var buildRoot = getBuildRoot(spec)
+    out, err := exec.Command(
+      "fakeroot",
+      "dpkg-deb",
+      "--build",
+      buildRoot).CombinedOutput()
+    if err != nil {
+        fmt.Printf("\nError build deb: %s\n", err.Error())
+        fmt.Printf("\nOut build deb: %s\n", out)
+        log.Fatal(err)
+    }
+    fmt.Printf("\nDeb build: %s\n", out)
+}
+
+type ChangeLog struct {
     Version string `version`
     Distribution string `distribution`
     Urgency string `urgency`
     Date string `date`
     Changes []string `changes`
-  }
+}
 
-  type Files struct {
+type Files struct {
     Path string `path`
     Defattr string `defattr`
-  }
+}
 
-	type SpecStruct struct {
-		Name string
+type SpecStruct struct {
+    Name string
     Version string
     Release string
+    Priority string
     Summary string
     Group string
+    BuildArch string
     License string
     URL string
+    Maintainer string
     Source0 string
     BuildRoot string
     BuildRequires string
@@ -66,20 +107,20 @@ func readJson() {
     Clean []string `clean`
     Files []Files `files`
     ChangeLog []ChangeLog `changelog`
-	}
+}
+
+
+func readJson() (spec SpecStruct){
+
   var specJson = readSpecFile()
 	dec := json.NewDecoder(strings.NewReader( specJson ))
-	for {
-		var spec SpecStruct
 		if err := dec.Decode(&spec); err == io.EOF {
-			break
+//			break
 		} else if err != nil {
 	    fmt.Printf("Error: %s\n", err)
 			log.Fatal(err)
 		}
-    fmt.Printf("install: %s\n", spec.Install[0])
-		fmt.Printf("Value: %s\n", spec.Name, spec)
-	}
+  return
 }
 
 func readSpecFile() (specData string){
@@ -88,7 +129,6 @@ func readSpecFile() (specData string){
         fmt.Printf("File error: %v\n", e)
         os.Exit(1)
     }
-    fmt.Printf("%s\n", string(file))
     specData=string(file)
     return
 }
